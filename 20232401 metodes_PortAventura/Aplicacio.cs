50ee22c0fe5be7569c6f475bc14bc0769a913e88
mbalaguerq <notin@MARCIMONI>
2023-01-24 21:49:36 +0100

    Agregar archivos de proyecto.

---
+++ 20232401 metodes_PortAventura/Aplicacio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20232401_metodes_PortAventura
+{
+    internal class Aplicacio
+    {
+        
+        String[,] alumnos = new String[50,2];
+        String[,] inscripciones = new String[50, 2];
+        String[,] actividades = { { "1, Salida a esquiar" }, { "2, Salida a Port Aventura" }, { "3,Feria Games World" }};
+    }
+}
