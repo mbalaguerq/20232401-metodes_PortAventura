f70ebbf0946546c391755a581275c391e89f4389
marc.balaguer.quintana@alumnes.ilernabcn.com <marc.balaguer.quintana@alumnes.ilernabcn.com>
2023-01-26 19:09:52 +0100

    Fet menu inici

--- 20232401 metodes_PortAventura/Aplicacio.cs
+++ 20232401 metodes_PortAventura/Aplicacio.cs
@@ -1,16 +1,102 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _20232401_metodes_PortAventura
 {
     internal class Aplicacio
     {
-        
-        String[,] alumnos = new String[50,2];
-        String[,] inscripciones = new String[50, 2];
-        String[,] actividades = { { "1, Salida a esquiar" }, { "2, Salida a Port Aventura" }, { "3,Feria Games World" }};
+        public void Inici()
+        {
+
+            String[,] alumnes = DadesAlumnes();
+            String[,] inscripcions = DadesInscripcions();
+            String[,] activitats = DadesActivitats();
+
+
+            bool salir = false;
+            string opcio;
+
+            do
+            {
+                mostrarMenu();
+                opcio = DemanarOpcioMenu();
+                salir = ExecutarMenu(alumnes, inscripcions, activitats, opcio);
+
+            } while (!salir);
+            Console.WriteLine();
+
+        }
+
+        string DemanarOpcioMenu()
+        {
+            string opcio;
+            do
+            {
+                Console.Write("Sel.lecciona opció: ");
+                opcio = Console.ReadLine();
+            } while (!"0123456789".Contains(opcio));
+            return opcio;
+        }
+
+
+        bool ExecutarMenu(String[,] alumnes, String[,] inscripcions, String[,] activitats, string opcio)
+        {
+            bool salir = false;
+            switch (opcio)
+            {
+                case "1":
+                    ;
+                    break;
+                case "0":
+                    salir = true;
+                    break;
+
+            }
+            return salir;
+        }
+
+        void mostrarMenu()
+        {
+            Console.WriteLine("1.  ");
+            Console.WriteLine("2. ");
+            Console.WriteLine("3. ");
+            Console.WriteLine("4. ");
+            Console.WriteLine("5. ");
+            Console.WriteLine("6. ");
+            Console.WriteLine("7. ");
+            Console.WriteLine("8. ");
+            Console.WriteLine("9. ");
+            Console.WriteLine("0. Sortir");
+        }
+
+
+
+        string[,] DadesAlumnes()
+        {
+            //nif/nom
+            String[,] alumnes = new String[50, 2];
+            return alumnes;
+        }
+        string[,] DadesInscripcions()
+        {
+            //nif/codi activitat
+            String[,] inscripcions = new String[50, 2];
+            return inscripcions;
+        }
+        string[,] DadesActivitats()
+        {
+            //codi activitat/nom activitat
+            String[,] activitats = {
+                    { "1, Salida a esquiar" }, { "2, Salida a Port Aventura" }, { "3,Feria Games World" } };
+            return activitats;
+        }
     }
-}
+}   
+
--- 20232401 metodes_PortAventura/Program.cs
+++ 20232401 metodes_PortAventura/Program.cs
@@ -10,6 +10,8 @@ namespace _20232401_metodes_PortAventura
     {
         static void Main(string[] args)
         {
+            Aplicacio app= new Aplicacio();
+            app.Inici();
         }
     }
 }
